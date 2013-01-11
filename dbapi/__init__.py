8be483977e13e87d4552bea17d943de5669433ff
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-11 06:59:07 +0000

    Store database in absolute path alongside database scripts
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@14 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/__init__.py
+++ dbapi/__init__.py
@@ -1,4 +1,9 @@
 import sqlite3
-conn = sqlite3.connect("database.db")
+import os.path
+
+# This is the directory of this file
+db_path = os.path.dirname(__file__)
+
+conn = sqlite3.connect(db_path + "/database.db")
 cur = conn.cursor()
-cur.executescript(open("setup.sql").read())
+cur.executescript(open(db_path + "/setup.sql").read())

1ccb377e5a8e3ef3f9897252ebd85944b6250b0e
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-11 06:33:55 +0000

    Add put method and test
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@5 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/user.py
+++ dbapi/user.py
@@ -13,27 +13,36 @@ class User(object):
         self.username = username
         self.password = password
 
-    def get(username):
+    @classmethod
+    def get(cls, username):
         cur = conn.cursor()
-        cur.executescript(open("something.sql").read())
-        cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+        cur.execute("SELECT id, firstname, lastname, username, password FROM users WHERE username = ?",
+                    (username,))
         for row in cur:
-            return row
-
+            return User(*row)
 
     def create(uid, fname, lname, username, password):
         cur = conn.cursor()
-        cur.executescript(open("something.sql").read())
         cur.execute(
             "INSERT INTO users VALUES (?, ?, ?, ?, ?);",
             (uid, fname, lname, username, password))
 
-    def put(fieldname, value):
-        #cur = conn.cursor()
-        #cur.executescript(open("something.sql").read())
-        #cur.execute()
-        if something is None:
-            pass
-        pass
-        
+    def update(self, fieldname, value):
+        cur = conn.cursor()
+        cur.execute("UPDATE users SET " + fieldname + "=? WHERE id = ?",
+                    (value, self.uid))
 
+    def put(self):
+        self.update('firstname', self.fname)
+        self.update('lastname', self.lname)
+        self.update('password', self.password)
+        #User name and id can't change
+
+if __name__ == "__main__":
+    #the test code
+    s = User.get('sdfko')
+    previous_name = s.fname
+    s.fname = "Sam"
+    s.put()
+    s2 = User.get('sdfko')
+    assert s2.fname != previous_name, "Name didn't change :("

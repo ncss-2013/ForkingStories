6431bbd2c35c32344f6150fe781bd0667a328a03
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-12 06:43:56 +0000

    Added date argument to make_time_float() to enable time comparisons in sqlite
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@56 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/dbtime.py
+++ dbapi/dbtime.py
@@ -13,7 +13,16 @@ import time
 format_str = '%Y-%m-%d %H:%M:%S.%f'
 
 def create_datetime(time_float):
-    return datetime.datetime.fromtimestamp(float(time_float))
+    return datetime.datetime.fromtimestamp(time_float)
     
-def make_time_float():
-    return time.time()
+def make_time_float(date=tuple()):
+    '''Returns in seconds since epoch. Calulates now if time = ().
+Otherwise, you can calulate a certain date by specifying
+(year, month, day)
+
+'''
+    if not date:
+        return time.time()
+    else:
+        return (datetime.date(date[0], date[1], date[2]) -
+                datetime.date(1970, 1, 1)).total_seconds()

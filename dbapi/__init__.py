47e4bf38ae333605c66fd6ac647dac1332f3c461
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-12 03:49:26 +0000

    import fix
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@28 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/__init__.py
+++ dbapi/__init__.py
@@ -1,9 +1,27 @@
 import sqlite3
 import os.path
+import sys
 
-# This is the directory of this file
-db_path = os.path.dirname(__file__)
+print("Running setup lol")
 
-conn = sqlite3.connect(db_path + "/database.db")
-cur = conn.cursor()
-cur.executescript(open(db_path + "/setup.sql").read())
+def _setup():
+  global conn
+  if hasattr(_setup, 'done'):
+    return
+
+  # Fix import paths for dbapi
+  # NOTE: This isn't particularly awesome, since multiple 'packages' like this
+  # will all try to win. It might be fine if we just have dbapi.
+  import_path = os.path.dirname(__file__)
+  if import_path not in sys.path:
+    sys.path.insert(0, import_path)
+
+  # This is the directory of this file
+  db_path = os.path.dirname(__file__)
+  conn = sqlite3.connect(db_path + "/database.db")
+  cur = conn.cursor()
+  cur.executescript(open(db_path + "/setup.sql").read())
+
+  _setup.done = True
+
+_setup()

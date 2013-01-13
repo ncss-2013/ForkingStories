b8c7bf1aab90b5ffc95f01c141d6498cedf8f3cb
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 00:13:02 +0000

    Added dummy "handler_chat.py" and import reference in server.py. James
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@72 077c64fb-b2d2-0410-9397-d79fc6128392

--- server.py
+++ server.py
@@ -6,24 +6,30 @@ import handler_login
 import handler_main
 import handler_story
 import handler_user
+import handler_chat
 
 # TODO
 #import handler_register
 
 server = Server()
 server.register('/', handler_main.index)
+
 server.register('/login', handler_login.login)
 server.register('/authenticate', handler_login.authenticate)
 server.register('/logout', handler_login.logout)
 server.register('/changepassword', handler_login.changepassword)
+
 server.register('/user/(\w[\w\d]+)', handler_user.user)
 server.register('/profiles', handler_user.profiles)
+
 server.register('/view_story', handler_story.view_story)
 server.register('/add_to_stories', handler_story.add_to_stories)
 server.register('/new_story', handler_story.new_story)
 server.register('/view_story_list', handler_story.view_story_list)
+
 # server.register('/register', register.register)
 # server.register('/process_register', register.process_register)
+
 server.run()
 
 

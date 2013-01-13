1b92e0a112d1cdbe6ecde1b088842934c6f7e4c5
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 01:47:08 +0000

    Reverted database date operations to text.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@88 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/dbtime.py
+++ dbapi/dbtime.py
@@ -7,5 +7,17 @@ format_str = '%Y-%m-%d %H:%M:%S.%f'
 def get_time_from_str(time_str):
     return datetime.datetime.strptime(time_str, format_str)
     
-def make_time_str():
-    return str(datetime.datetime.today())
+def make_time_str(date=tuple()):
+    '''Date can be (<year>, <month>, <day>)'''
+    if not date:
+        return datetime.datetime.today().strftime(format_str)
+    else:
+        return datetime.datetime(date[0], date[1], date[2]).strftime(format_str)
+
+if __name__ == '__main__':
+    assert get_time_from_str('2013-01-13 12:36:01.456000') == \
+           datetime.datetime(2013, 1, 13, 12, 36, 1, 456000)
+    assert get_time_from_str('2013-01-13 12:36:01.456000').second == 1
+    assert make_time_str((1997, 12, 11)) == '1997-12-11 00:00:00.000000'
+    assert isinstance(get_time_from_str(make_time_str((1997, 12, 11))),
+                      datetime.datetime)

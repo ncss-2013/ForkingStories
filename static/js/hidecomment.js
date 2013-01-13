8fb794657f8f4bd5b57bed6530347e2f7af98070
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 11:26:14 +0000

    Added comments for viewing a story. And some other UI tweaks.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@151 077c64fb-b2d2-0410-9397-d79fc6128392

---
+++ static/js/hidecomment.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#hidecomment').click( function(event){
+            if ($('#comment').is(':visible')){
+            $('#hidecomment').val('Leave a comment')
+            }
+            else {
+            $('#hidecomment').val('Leave a comment')
+            }
+            $('#comment').toggle();  
+                
+    });
+})

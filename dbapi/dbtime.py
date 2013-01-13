e9e5e7e35184c559da4c51a9d74ae9a9788dc71f
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 01:32:01 +0000

    Updated date functions to text (not floats).
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@84 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/dbtime.py
+++ dbapi/dbtime.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python3
-'''Contains some conveinience functions for managing datetimes.
-
-These functions should be used internally across the entire dbapi.
-
-Written by Alex Mueller
-
-'''
 
 import datetime
-import time
 
 format_str = '%Y-%m-%d %H:%M:%S.%f'
 
-def create_datetime(time_float):
-    return datetime.datetime.fromtimestamp(float(time_float))
+def get_time_from_str(time_str):
+    return datetime.datetime.strptime(time_str, format_str)
     
-def make_time_float(date=tuple()):
-    '''Returns in seconds since epoch. Calulates now if time = ().
-Otherwise, you can calulate a certain date by specifying
-(year, month, day)
-
-'''
-    if not date:
-        return time.time()
-    else:
-        return (datetime.date(date[0], date[1], date[2]) -
-                datetime.date(1970, 1, 1)).total_seconds()
+def make_time_str():
+    return str(datetime.datetime.today())

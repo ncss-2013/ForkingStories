6b0a87af253ead1329185ee716ebe82dd26ca8cd
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 15:10:43 +0000

    Hopefully fixed all the things this time.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@195 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/setup.sql
+++ dbapi/setup.sql
@@ -94,7 +94,7 @@ REPLACE INTO paragraphs (id, content, parent_id, votes, author_id, approved, sto
 REPLACE INTO paragraphs (id, content, parent_id, votes, author_id, approved, story_id, created) 
     VALUES (6, "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, 
     thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because 
-    they just didn’t hold with such nonsense. Mr. Dursley was the director of a firm called Grunnings, which made drills. 
+    they just didn't hold with such nonsense. Mr. Dursley was the director of a firm called Grunnings, which made drills. 
     He was a big, beefy man with hardly any neck, although he did have a very large mustache. Mrs. Dursley was thin and 
     blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her time craning 
     over garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in their opinion there was 

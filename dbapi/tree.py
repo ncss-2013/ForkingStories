2e256814fdd0bbc9c4d5517899de47c82c9921d9
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 12:38:59 +0000

    Fixed tree.py to return the root node of the tree - Alex
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@176 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/tree.py
+++ dbapi/tree.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-'''Has functions which can be used to create a tree of Paragraph objects.'''
+'''Has functions which can be used to create a tree of Paragraph objects.
+
+Written by Alex Mueller with a lot of direction from James
+
+'''
 
 import __importfix__; __package__ = 'dbapi'
 from .__init__ import *
@@ -14,19 +18,37 @@ class Node(object):
         self.children.append(paragraph)
 
 def create_tree(story):
+    # NOTE TO JAMES: There are probably 10 times more efficent and more
+    # pythonic ways to write this functions. Please point them out to me
+    # if any come to mind. I am eager to become a better programmer!
     from dbapi.paragraph import Paragraph
     paragraphs = Paragraph.get_approved_paragraphs(story.id)
     tree = dict()
+    
+    # Create dict of nodes
+    
     for p in paragraphs:
         tree[p.id] = Node(p)
+        
+    # Fill out the node data
+    
     for p in paragraphs:
         parent = tree[p.parent_id] if p.parent_id != -1 else None
         tree[p.id].parent = \
             parent if p.parent_id != -1 else None
         if p.parent_id != -1:
             tree[p.parent_id].children.append(tree[p.id])
-    return tree[0]
+    # Find and return the root node
+    
+    # Get the node that is probably closet to the root of the dictionary
+    a_node = tree[list(tree.values())[0].paragraph.id]
+    # Loop up the tree to the root node
+    while a_node.parent != -1:
+        a_node = a_node.parent
+    return a_node
+    
 
 if __name__ == '__main__':
     from dbapi.story import Story
+    print(create_tree(Story.find('id', 0)[0]))
     

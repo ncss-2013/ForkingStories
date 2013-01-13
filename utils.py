03bcb584a6925dda80eb369e6cb445d0cb486d05
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 14:09:27 +0000

    Added utility file
    ~ Dom
    
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@193 077c64fb-b2d2-0410-9397-d79fc6128392

---
+++ utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+def get_error(response, dest):
+    r = (
+        response.get_secure_cookie(dest) or b'').decode()
+    response.clear_cookie(dest)
+    return r
+
+
+def set_error(response, k_v):
+    for key, value in k_v.items():
+        response.set_secure_cookie(key, value)
+        assert response.get_secure_cookie(key) == value
+    return response

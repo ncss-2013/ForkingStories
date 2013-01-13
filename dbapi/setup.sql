9b5dcffec5b4d1391321226c453c2f21d58a8d1d
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 14:06:54 +0000

    fixed all the things!!!!!
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@191 077c64fb-b2d2-0410-9397-d79fc6128392

--- dbapi/setup.sql
+++ dbapi/setup.sql
@@ -93,17 +93,17 @@ REPLACE INTO paragraphs (id, content, parent_id, votes, author_id, approved, sto
     VALUES (5, "Aragorn!", 2, 10, 0, 1, 0, "2013-01-13 16:24:27.000000");   
 REPLACE INTO paragraphs (id, content, parent_id, votes, author_id, approved, story_id, created) 
     VALUES (6, "Mr. and Mrs. Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, 
-    thank you very much. They were the last people you’d expect to be involved in anything strange or mysterious, because 
+    thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because 
     they just didn’t hold with such nonsense. Mr. Dursley was the director of a firm called Grunnings, which made drills. 
     He was a big, beefy man with hardly any neck, although he did have a very large mustache. Mrs. Dursley was thin and 
     blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her time craning 
     over garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in their opinion there was 
     no finer boy anywhere. The Dursleys had everything they wanted, but they also had a secret, and their greatest fear 
-    was that somebody would discover it. They didn’t think they could bear it if anyone found out about the Potters.", 
+    was that somebody would discover it. They didn't think they could bear it if anyone found out about the Potters.", 
     -1, 10, 2, 1, 1, "2013-01-13 16:24:27.000000");
 REPLACE INTO paragraphs (id, content, parent_id, votes, author_id, approved, story_id, created) 
-    VALUES (7, "Mrs. Potter was Mrs. Dursley’s sister, but they hadn’t met for several years; in fact, Mrs. Dursley 
-    pretended she didn’t have a sister, because her sister and her good-for-nothing husband were as unDursleyish as 
+    VALUES (7, "Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley 
+    pretended she didn't have a sister, because her sister and her good-for-nothing husband were as unDursleyish as 
     it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the 
     street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him.", 
     6, 10, 2, 1, 1, "2013-01-13 16:24:27.000000");

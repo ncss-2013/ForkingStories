eecffd556620642bf561c5257fbe86a790a0af08
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 11:38:40 +0000

    Added password popover for registation (validation). Added form verification.
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@157 077c64fb-b2d2-0410-9397-d79fc6128392

--- static/js/utility.js
+++ static/js/utility.js
@@ -1,15 +1,37 @@
-    /* password checker */
+
+/* this global variable will contain the HTML element with a message in it
+   when people enter the wrong password */
+var tooltip = null;
+
+/* password checker */
 function validate_passwords(pass1, pass2){
     $('#' + pass2 + ', #' + pass1).change(function(){
         var password = $('#' + pass1).val();
         var passwordrepeat = $('#' + pass2).val();
+        
+        // remove old tooltips
+        if (tooltip)
+            tooltip.remove();
+        
         if (password == '' || passwordrepeat == ''){
             $('#' + pass2 + ', #' + pass1).removeClass('valid-input invalid-input');
+            tooltip = $("<div class='tooltip'>").text("Please enter a password");
         } else if (password != passwordrepeat){
             $('#' + pass2 + ', #' + pass1).switchClass('valid-input', 'invalid-input',0);
+            tooltip = $("<div class='tooltip'>").text("Make sure your passwords match");
         } else {
             $('#' + pass2 + ', #' + pass1).switchClass('invalid-input', 'valid-input',0);
+            tooltip = null;
         }
+        
+        if (tooltip)
+            $("body").append(tooltip
+            
+                .css({
+                    top: "378px",
+                    left: "589px"
+                })
+            );
     })
 }
 

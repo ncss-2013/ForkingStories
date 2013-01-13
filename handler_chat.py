c6043b277661755e5ad76bf16d45572f57ac95e3
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-13 00:41:41 +0000

    Added base chat file -James
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@78 077c64fb-b2d2-0410-9397-d79fc6128392

--- handler_chat.py
+++ handler_chat.py
@@ -1,1 +1,35 @@
-# TODO:  Module for implementing chat.
+#!/usr/bin/env python3.3
+
+class ConnectionHandler(object):
+  @classmethod 
+  def register(cls, connection):
+    pass
+  
+  @classmethod
+  def sendMsg(cls, connection, msg):
+    pass
+    # foreach user talking in connection.channel
+    #    call onMessageRecv
+  
+  @classmethod
+  def unregister(cls, connection):
+    pass
+    #....
+
+  foo = set()
+
+class Connection(object):
+  def __init__(self, name, channel):
+    #....
+    self.name = name
+    self.channel = channel
+    ConnectionHandler.register(self)
+
+  def onMessageRecv(self, msg):
+    pass
+    # websocket stuff
+
+  def onMessageSend(self, msg):
+    ConnectionHandler.sendMsg(self, msg)
+
+

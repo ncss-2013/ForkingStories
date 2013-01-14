35997cfd89dc516fd01a7a3c6a4f97d1bc306de9
ncss132 <ncss132@077c64fb-b2d2-0410-9397-d79fc6128392>
2013-01-14 02:42:50 +0000

    Fixed comments!
    git-svn-id: http://svn.ask.it.usyd.edu.au/ncss132@278 077c64fb-b2d2-0410-9397-d79fc6128392

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import handler_login
 import handler_main
 import handler_story
 import handler_story_tree
+import handler_comment
 import handler_user
 # import handler_chat
 import handler_register
@@ -26,6 +27,8 @@ server.register('/check_username/(\w[\w\d]+)', handler_user.check_username)
 
 server.register('/view_story/([0-9]+)', handler_story_tree.display_story_tree)
 server.register('/add_to_story/(\d+)', handler_story_tree.add_to_story_tree)
+
+server.register('/add_comment/([0-9]+)', handler_comment.add_comment)
 
 server.register('/new_story', handler_story.new_story)
 server.register('/process_new_story', handler_story.process_new_story)
